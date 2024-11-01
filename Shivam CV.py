be475367a22cfbb904f02bdde06a1c61bdb1cdf4
Shivam7979 <soni.shivam1303@gmail.com>
2024-11-01 20:01:28 +0000

    Create Shivam CV.py

---
+++ Shivam CV.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Shivam Soni CV",
+    page_icon=":wave:",
+    layout="centered"
+)
+
+# Set a light theme background
+st.config_theme(page_icon=False, page_backgroundColor="#FFFFFF")
